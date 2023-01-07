dbfe8575e5de9f2a29e42e646949b417993bb914
Aleksei Zverev <9294414@gmail.com>
2023-01-07 19:45:53 +0100

    United positive and negative tests

--- src/test/java/ru/netology/OrdercardTest.java
+++ src/test/java/ru/netology/OrdercardTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OrdercardTest {
     private WebDriver driver;
@@ -81,6 +82,64 @@ public class OrdercardTest {
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id = order-success]")).getText().trim();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTestV5() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id = name] input")).sendKeys("Aleksei Zverev");
+        driver.findElement(By.cssSelector("[data-test-id = phone] input")).sendKeys("+79119294414");
+        driver.findElement(By.cssSelector("[data-test-id = agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id = name].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTestV6() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id = name] input")).clear();
+        driver.findElement(By.cssSelector("[data-test-id = phone] input")).sendKeys("+79119294414");
+        driver.findElement(By.cssSelector("[data-test-id = agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.cssSelector("[data-test-id = name].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTestV7() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id = name] input")).sendKeys("Алексей Зверев");
+        driver.findElement(By.cssSelector("[data-test-id = phone] input")).sendKeys("+791119294414");
+        driver.findElement(By.cssSelector("[data-test-id = agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id = phone].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTestV8() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id = name] input")).sendKeys("Алексей Зверев");
+        driver.findElement(By.cssSelector("[data-test-id = phone] input")).clear();
+        driver.findElement(By.cssSelector("[data-test-id = agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.cssSelector("[data-test-id = phone].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldTestV9() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id = name] input")).sendKeys("Алексей Зверев");
+        driver.findElement(By.cssSelector("[data-test-id = phone] input")).sendKeys("+79119294414");
+        driver.findElement(By.className("button")).click();
+        boolean actual = driver.findElement(By.cssSelector("[data-test-id = agreement].input_invalid .checkbox__text")).isDisplayed();
+        assertTrue(actual);
     }
 
 }
